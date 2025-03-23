5b47e7637f54a9ec2ae52bb7a784e629edf11a45
Vonng <rh@vonng.com>
2025-03-23 19:11:19 +0800

    fix pgbackrest_exporter issue

--- arm64/pgbackrest_exporter/src/preinstall.sh
+++ arm64/pgbackrest_exporter/src/preinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # create a group & user named prometheus if not exists
-getent group prometheus >/dev/null || groupadd -r prometheus ; /bin/true
-getent passwd prometheus >/dev/null || useradd -r -g prometheus -s /sbin/nologin -c "Prometheus services" prometheus
-exit 0
+# getent group prometheus >/dev/null || groupadd -r prometheus ; /bin/true
+# getent passwd prometheus >/dev/null || useradd -r -g prometheus -s /sbin/nologin -c "Prometheus services" prometheus
+# exit 0
