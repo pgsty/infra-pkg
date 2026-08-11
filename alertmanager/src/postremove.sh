dacda9f88ee8924e3225d6061c46b7bcf225d8a0
Feng Ruohang <rh@vonng.com>
2026-08-11 23:35:06 +0800

    alertmanager: normalize service package lifecycle

--- alertmanager/src/postremove.sh
+++ alertmanager/src/postremove.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-if [ "${DPKG_MAINTSCRIPT_NAME:-}" = "postrm" ] && command -v dpkg-maintscript-helper >/dev/null 2>&1; then
-    dpkg-maintscript-helper rm_conffile /lib/systemd/system/alertmanager.service 0.33.1-2~ alertmanager -- "$@" || exit 1
-fi
-
 case "$1" in
     purge)
         if [ -L /etc/systemd/system/multi-user.target.wants/alertmanager.service ]; then
--- alertmanager/src/preinstall.sh
+++ alertmanager/src/preinstall.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-if [ "${DPKG_MAINTSCRIPT_NAME:-}" = "preinst" ] && command -v dpkg-maintscript-helper >/dev/null 2>&1; then
-    dpkg-maintscript-helper rm_conffile /lib/systemd/system/alertmanager.service 0.33.1-2~ alertmanager -- "$@" || exit 1
-fi
-
 if ! getent group prometheus >/dev/null 2>&1; then
     groupadd -r prometheus || exit 1
 fi
