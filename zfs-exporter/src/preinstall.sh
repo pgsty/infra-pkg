3e25528b522995f54d6b2eecaedd73cebcc991b0
Feng Ruohang <rh@vonng.com>
2026-08-11 23:36:59 +0800

    zfs-exporter: rename and normalize package recipe

--- zfs-exporter/src/preinstall.sh
+++ zfs-exporter/src/preinstall.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-if [ "${DPKG_MAINTSCRIPT_NAME:-}" = "preinst" ] && command -v dpkg-maintscript-helper >/dev/null 2>&1; then
-    dpkg-maintscript-helper rm_conffile /lib/systemd/system/zfs_exporter.service 3.8.1-2~ zfs-exporter -- "$@" || exit 1
-fi
-
 if ! getent group prometheus >/dev/null 2>&1; then
     groupadd -r prometheus || exit 1
 fi
