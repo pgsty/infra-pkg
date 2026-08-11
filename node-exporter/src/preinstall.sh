b693e45a84308c91e255aa2c72ae6d518f77363f
Feng Ruohang <rh@vonng.com>
2026-08-11 23:36:04 +0800

    node-exporter: rename and normalize package recipe

--- node-exporter/src/preinstall.sh
+++ node-exporter/src/preinstall.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-if [ "${DPKG_MAINTSCRIPT_NAME:-}" = "preinst" ] && command -v dpkg-maintscript-helper >/dev/null 2>&1; then
-    dpkg-maintscript-helper rm_conffile /lib/systemd/system/node_exporter.service 1.12.1-2~ node-exporter -- "$@" || exit 1
-fi
-
 if ! getent group prometheus >/dev/null 2>&1; then
     groupadd -r prometheus || exit 1
 fi
