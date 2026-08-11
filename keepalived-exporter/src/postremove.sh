256690c507e18c479747033abe763da7aefdd496
Feng Ruohang <rh@vonng.com>
2026-08-11 23:35:34 +0800

    keepalived-exporter: rename the independent monitor package

--- keepalived-exporter/src/postremove.sh
+++ keepalived-exporter/src/postremove.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-if [ "${DPKG_MAINTSCRIPT_NAME:-}" = "postrm" ] && command -v dpkg-maintscript-helper >/dev/null 2>&1; then
-    dpkg-maintscript-helper rm_conffile /lib/systemd/system/keepalived_exporter.service 1.7.1-2~ keepalived-exporter -- "$@" || exit 1
-fi
-
 case "$1" in
     purge)
         if [ -L /etc/systemd/system/multi-user.target.wants/keepalived_exporter.service ]; then
--- keepalived_exporter/src/preinstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ "${DPKG_MAINTSCRIPT_NAME:-}" = "preinst" ] && command -v dpkg-maintscript-helper >/dev/null 2>&1; then
-    dpkg-maintscript-helper rm_conffile /lib/systemd/system/keepalived_exporter.service 1.7.1-2~ keepalived-exporter -- "$@" || exit 1
-fi
-
-exit 0
