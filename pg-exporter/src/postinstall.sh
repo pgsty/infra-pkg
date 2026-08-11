450ad0fcb20b7b55ff62c4d9e1316714c729c7fb
Feng Ruohang <rh@vonng.com>
2026-08-11 23:36:05 +0800

    pg-exporter: rename and normalize package recipe

--- pg-exporter/src/postinstall.sh
+++ pg-exporter/src/postinstall.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-if [ "${DPKG_MAINTSCRIPT_NAME:-}" = "postinst" ] && command -v dpkg-maintscript-helper >/dev/null 2>&1; then
-    dpkg-maintscript-helper rm_conffile /lib/systemd/system/pg_exporter.service 1.4.1-2~ pg-exporter -- "$@" || exit 1
-fi
-
 if command -v systemctl >/dev/null 2>&1; then
     systemctl daemon-reload >/dev/null 2>&1 || :
 fi
 
-case "$1" in
-    1)
+case "${1:-}:${2:-}" in
+    1:*|configure:)
         if command -v systemctl >/dev/null 2>&1; then
             systemctl --no-reload preset pg_exporter.service >/dev/null 2>&1 || :
         fi
