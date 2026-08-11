d74e15acb0d6f02862a3f4416946d93d738ba884
Feng Ruohang <rh@vonng.com>
2026-08-11 23:36:04 +0800

    mongodb-exporter: rename and normalize package recipe

--- mongodb-exporter/src/postremove.sh
+++ mongodb-exporter/src/postremove.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-if [ "${DPKG_MAINTSCRIPT_NAME:-}" = "postrm" ] && command -v dpkg-maintscript-helper >/dev/null 2>&1; then
-    dpkg-maintscript-helper rm_conffile /lib/systemd/system/mongodb_exporter.service 0.52.0-1~ mongodb-exporter -- "$@" || exit 1
-fi
-
 case "$1" in
     purge)
         if [ -L /etc/systemd/system/multi-user.target.wants/mongodb_exporter.service ]; then
