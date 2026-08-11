fd8370aca44459ffaca6a6d0aeea7b8b1b6263f7
Feng Ruohang <rh@vonng.com>
2026-08-11 23:36:59 +0800

    v2ray: restore the upstream package name

--- v2ray/src/preinstall.sh
+++ v2ray/src/preinstall.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-if [ "${DPKG_MAINTSCRIPT_NAME:-}" = "preinst" ] && command -v dpkg-maintscript-helper >/dev/null 2>&1; then
-    dpkg-maintscript-helper rm_conffile /lib/systemd/system/v2ray.service 5.51.2-2~ vray -- "$@" || exit 1
-fi
-
 if ! getent group v2ray >/dev/null 2>&1; then
     groupadd -r v2ray || exit 1
 fi
