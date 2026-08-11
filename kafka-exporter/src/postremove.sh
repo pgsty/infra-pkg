00d714f568918ddb960fbbe2a8a0796a029de2ea
Feng Ruohang <rh@vonng.com>
2026-08-11 23:35:34 +0800

    kafka-exporter: rename and normalize package recipe

--- kafka-exporter/src/postremove.sh
+++ kafka-exporter/src/postremove.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-if [ "${DPKG_MAINTSCRIPT_NAME:-}" = "postrm" ] && command -v dpkg-maintscript-helper >/dev/null 2>&1; then
-    dpkg-maintscript-helper rm_conffile /lib/systemd/system/kafka_exporter.service 1.9.0-2~ kafka-exporter -- "$@" || exit 1
-fi
-
 case "$1" in
     purge)
         if [ -L /etc/systemd/system/multi-user.target.wants/kafka_exporter.service ]; then
