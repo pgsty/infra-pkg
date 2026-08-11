a89e38c7a91de5d1c762c24bb6cad552ffc55d48
Feng Ruohang <rh@vonng.com>
2026-08-11 23:36:05 +0800

    postgrest: normalize service package lifecycle

--- postgrest/src/postremove.sh
+++ postgrest/src/postremove.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-if [ "${DPKG_MAINTSCRIPT_NAME:-}" = "postrm" ] && command -v dpkg-maintscript-helper >/dev/null 2>&1; then
-    dpkg-maintscript-helper rm_conffile /lib/systemd/system/postgrest.service 14.16.0-2~ postgrest -- "$@" || exit 1
-fi
-
 case "$1" in
     purge)
         if [ -L /etc/systemd/system/multi-user.target.wants/postgrest.service ]; then
--- postgrest/src/preinstall.sh
+++ postgrest/src/preinstall.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-if [ "${DPKG_MAINTSCRIPT_NAME:-}" = "preinst" ] && command -v dpkg-maintscript-helper >/dev/null 2>&1; then
-    dpkg-maintscript-helper rm_conffile /lib/systemd/system/postgrest.service 14.16.0-2~ postgrest -- "$@" || exit 1
-fi
-
 if ! getent group postgrest >/dev/null 2>&1; then
     groupadd -r postgrest || exit 1
 fi
