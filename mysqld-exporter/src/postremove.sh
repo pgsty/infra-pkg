793e05d483f4e36243f981b3c0f61ae2a37482ab
Feng Ruohang <rh@vonng.com>
2026-08-11 23:36:04 +0800

    mysqld-exporter: rename and normalize package recipe

--- mysqld-exporter/src/postremove.sh
+++ mysqld-exporter/src/postremove.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-if [ "${DPKG_MAINTSCRIPT_NAME:-}" = "postrm" ] && command -v dpkg-maintscript-helper >/dev/null 2>&1; then
-    dpkg-maintscript-helper rm_conffile /lib/systemd/system/mysqld_exporter.service 0.19.0-2~ mysqld-exporter -- "$@" || exit 1
-fi
-
 case "$1" in
     purge)
         if [ -L /etc/systemd/system/multi-user.target.wants/mysqld_exporter.service ]; then
--- mysqld-exporter/src/preinstall.sh
+++ mysqld-exporter/src/preinstall.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-if [ "${DPKG_MAINTSCRIPT_NAME:-}" = "preinst" ] && command -v dpkg-maintscript-helper >/dev/null 2>&1; then
-    dpkg-maintscript-helper rm_conffile /lib/systemd/system/mysqld_exporter.service 0.19.0-2~ mysqld-exporter -- "$@" || exit 1
-fi
-
 if ! getent group prometheus >/dev/null 2>&1; then
     groupadd -r prometheus || exit 1
 fi
